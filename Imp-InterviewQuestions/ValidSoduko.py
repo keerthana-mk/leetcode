4eb375cd52f78f09400128cffb315dd257abb8ad
keerthana-mk <keerthana271995@gmail.com>
2022-06-16 20:07:56 -0400

    16Jun2022: valid sudok

--- Imp-InterviewQuestions/ValidSoduko.py
+++ Imp-InterviewQuestions/ValidSoduko.py
@@ -8,13 +8,27 @@
 #
 # A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 # Only the filled cells need to be validated according to the mentioned rules.
+import collections
+
 
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        return checkrows(board)
-        def checkrows(board):
-            for i in range(0, len(board)):
-                print(board[i][0])
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]:
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
 
 
 s= Solution()
@@ -27,5 +41,13 @@ board = [["5","3",".",".","7",".",".",".","."]
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]
-
-s.isValidSudoku(board)
+board1 =[["8","3",".",".","7",".",".",".","."],
+         ["6",".",".","1","9","5",".",".","."],
+         [".","9","8",".",".",".",".","6","."],
+         ["8",".",".",".","6",".",".",".","3"],
+         ["4",".",".","8",".","3",".",".","1"],
+         ["7",".",".",".","2",".",".",".","6"],
+         [".","6",".",".",".",".","2","8","."],
+         [".",".",".","4","1","9",".",".","5"],
+         [".",".",".",".","8",".",".","7","9"]]
+print(s.isValidSudoku(board1))
